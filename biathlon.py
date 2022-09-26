84e92dca415acdc2c8a666c3c4509833d690a122
Marre3 <marre244@gmail.com>
2022-09-26 21:51:49 +0200

    Räkna antal träffade mål

--- biathlon.py
+++ biathlon.py
@@ -34,6 +34,10 @@ def close_target(n, targets):
 	return targets
 
 
+def hits(targets):
+	return len([t for t in targets if is_closed(t)])
+
+
 def splash():
 	print(splash_str)
 
@@ -53,8 +57,10 @@ print(is_open(open()))
 print(is_closed(open()))
 ts = new_targets()
 print(ts)
+print(f"hits: {hits(ts)}")
 for x in ts:
 	print(is_open(x), is_closed(x))
 
 ts = close_target(2, ts)
 print(ts)
+print(f"hits: {hits(ts)}")
