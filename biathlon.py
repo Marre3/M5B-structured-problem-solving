27c67e8bf3301575c58b39f146567e3a47aae877
Marre3 <marre244@gmail.com>
2022-09-26 21:44:16 +0200

    Grundläggande funktioner

--- biathlon.py
+++ biathlon.py
@@ -8,6 +8,27 @@ splash_str = """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
+
+def open():
+	return 0
+
+
+def closed():
+	return 1
+
+
+def is_open(target):
+	return target == open()
+
+
+def is_closed(target):
+	return target == closed()
+
+
+def new_targets():
+	return [open()] * 5
+
+
 def splash():
 	print(splash_str)
 
@@ -18,3 +39,13 @@ def main():
 
 if __name__ == "__main__":
 	main()
+
+
+## Test
+print(open())
+print(closed())
+print(is_open(open()))
+print(is_closed(open()))
+print(new_targets())
+for x in new_targets():
+	print(is_open(x), is_closed(x))
