f02ee68d6730abe283b9e7d5db242519a3ce65a1
Marre3 <marre244@gmail.com>
2022-09-26 21:31:30 +0200

    Splash screen

---
+++ biathlon.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+splash_str = """
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+              Biathlon
+
+         a hit or miss game
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+def splash():
+	print(splash_str)
+
+
+def main():
+	splash()
+
+
+if __name__ == "__main__":
+	main()
